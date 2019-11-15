c806cc0ac3c06af9c69f0faa60a00ab5b8f49d37
Sepi <deet1022@gmail.com>
2019-11-15 16:19:22 +0900

    Create project

---
+++ src/CopyBase/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CopyBase
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
