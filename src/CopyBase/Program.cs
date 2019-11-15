4f136ed0459052d95e9f3f7a1cb90f62fdb57abc
Sepi <deet1022@gmail.com>
2019-11-15 23:18:57 +0900

    Add NuGet packages

--- src/CopyBase/Program.cs
+++ src/CopyBase/Program.cs
@@ -1,12 +1,27 @@
 ﻿using System;
+using System.Text;
+using Figgle;
 
 namespace CopyBase
 {
     class Program
     {
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var figgle = FiggleFonts.Standard.Render("CopyBase");
+            Console.WriteLine(figgle);
+        }
+
+        private static void DisplayTextLine(int repeat)
+        {
+            var builder = new StringBuilder();
+            for (int i = 0; i < repeat; i++)
+            {
+                builder.Append('=');
+            }
+
+            Console.WriteLine(builder.ToString());
         }
     }
 }
